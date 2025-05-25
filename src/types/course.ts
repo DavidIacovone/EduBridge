8fdb6f4b808163b57c5f7fa02c515b53a3d51a49
DavidIacovone <iacovone.david@gmail.com>
2025-05-25 15:49:35 +0200

    Added types for user and course

---
+++ src/types/course.ts
@@ -0,0 +1,10 @@
+export interface Course {
+    id: string;
+    city: string;
+    name: string;
+    description: string;
+    price: number;
+    subject: string;
+    email: string;
+    userId: string;
+}
